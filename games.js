456f0a81403e3ba19760660701d36b3675586cd7
Minhaj <minusahab69@gmail.com>
2026-06-26 21:23:07 +0500

    first commit

---
+++ games.js
@@ -0,0 +1,5 @@
+const TTTBtn = document.querySelector(".card1");
+
+TTTBtn.addEventListener("click", () => {
+    window.location.href = "tic-tac-toe.html";
+});
---
+++ startpage.js
@@ -0,0 +1,6 @@
+
+const startBtn = document.querySelector(".start");
+
+startBtn.addEventListener("click", () => {
+    window.location.href = "games.html";
+});
