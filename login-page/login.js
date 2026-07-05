5ddfc63e7400426fd5c71a47285c2820b36d788c
Minhaj <minusahab69@gmail.com>
2026-07-05 23:47:02 +0500

    added half login-page logic

--- login-page/login.js
+++ login-page/login.js
@@ -1,20 +1,34 @@
-let userName = document.querySelector(".name")
-let userPass = document.querySelector(".pass")
-let signupBtn = document.querySelector(".sign-up")
-let loginBtn = document.querySelector(".login")
+const userNameInput = document.querySelector(".name");
+const userPassInput = document.querySelector(".pass");
+const signupBtn = document.querySelector(".sign-up");
+const conditionsName = {
+    digits : document.querySelector(".N1"),
+    special : document.querySelector(".N2")
+}
+const conditionsPass = {
+    digits : document.querySelector(".P1"),
+    special : document.querySelector(".P2"),
+    alphabets : document.querySelector(".P3")
+}
+const specialChar = ["!", "#", "$", "%", "@", "^", "<", ">", ",", "/", "?", "+", "=", "-", "[", "]", "{", "}", "|", ":", ";", "&", "*", "(", ")", "`", "~"];
 
+signupBtn.addEventListener("click", () => {
 
-signupBtn.addEventListener("click" , ()=>{
-    if(userName.value && userPass.value){
-        let userInfo = {
-            userName : userName.value,
-            userPass : userPass.value
+    const userName = userNameInput.value.replaceAll(" ", "");
+    const userPass = userPassInput.value.replaceAll(" ", "");
+    const userNameArr = userName.split("");
+    const userPassArr = userPass.split("");
+
+    if (userName && userPass) {
+        console.log(userNameArr.some(item => item.includes(specialChar)));
+        if (userNameArr.length >= 8 && userPassArr.length >= 8 && !userNameArr.some(item => specialChar.includes(item))) {
+            let userInfo = {
+                userName,
+                userPass
+            };
+            console.log(userName.split(""));
         }
-        localStorage.setItem('user-info', JSON.stringify(userInfo))
-        console.log(userInfo.userName);
-    }else{
-        alert("please enter password and user name")
+    } else {
+        alert("please enter password and user name");
     }
-})
-
-// localStorage.clear()
+});
