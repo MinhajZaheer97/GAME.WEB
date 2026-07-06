07ab7be0a1f60b8209ac93f8bda16f0d3f3e82d8
Minhaj <minusahab69@gmail.com>
2026-07-07 00:18:53 +0500

    login page completed

--- login-page/login.js
+++ login-page/login.js
@@ -1,34 +1,111 @@
 const userNameInput = document.querySelector(".name");
 const userPassInput = document.querySelector(".pass");
 const signupBtn = document.querySelector(".sign-up");
+const nameCon = document.querySelector(".name-con");
+const passCon = document.querySelector(".pass-con");
+const showHidePass = document.querySelector(".show-pass");
+const img = showHidePass.querySelector("img");
 const conditionsName = {
-    digits : document.querySelector(".N1"),
-    special : document.querySelector(".N2")
-}
+  digits: document.querySelector(".N1"),
+  special: document.querySelector(".N2"),
+};
 const conditionsPass = {
-    digits : document.querySelector(".P1"),
-    special : document.querySelector(".P2"),
-    alphabets : document.querySelector(".P3")
-}
-const specialChar = ["!", "#", "$", "%", "@", "^", "<", ">", ",", "/", "?", "+", "=", "-", "[", "]", "{", "}", "|", ":", ";", "&", "*", "(", ")", "`", "~"];
+  digits: document.querySelector(".P1"),
+  special: document.querySelector(".P2"),
+};
+const specialChar = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "@",
+  "^",
+  "<",
+  ">",
+  ",",
+  "/",
+  "?",
+  "+",
+  "=",
+  "-",
+  "[",
+  "]",
+  "{",
+  "}",
+  "|",
+  ":",
+  ";",
+  "&",
+  "*",
+  "(",
+  ")",
+  "`",
+  "~",
+];
 
 signupBtn.addEventListener("click", () => {
+  const userName = userNameInput.value.replaceAll(" ", "");
+  const userPass = userPassInput.value.replaceAll(" ", "");
+  const userNameArr = userName.split("");
+  const userPassArr = userPass.split("");
 
-    const userName = userNameInput.value.replaceAll(" ", "");
-    const userPass = userPassInput.value.replaceAll(" ", "");
-    const userNameArr = userName.split("");
-    const userPassArr = userPass.split("");
+  const conditions = {
+    condition1: userNameArr.length >= 8,
+    condition2: userPassArr.length >= 8,
+    condition3: !userNameArr.some((item) => specialChar.includes(item)),
+    condition4: userPassArr.some((item) => specialChar.includes(item)),
+  };
 
-    if (userName && userPass) {
-        console.log(userNameArr.some(item => item.includes(specialChar)));
-        if (userNameArr.length >= 8 && userPassArr.length >= 8 && !userNameArr.some(item => specialChar.includes(item))) {
-            let userInfo = {
-                userName,
-                userPass
-            };
-            console.log(userName.split(""));
-        }
+  if (userName && userPass) {
+    if (conditions.condition1) {
+      conditionsName.digits.style.color = "green";
+      nameCon.style.display = "block";
     } else {
-        alert("please enter password and user name");
+      conditionsName.digits.style.color = "red";
+      nameCon.style.display = "block";
     }
+    if (conditions.condition2) {
+      conditionsPass.digits.style.color = "green";
+      passCon.style.display = "block";
+    } else {
+      conditionsPass.digits.style.color = "red";
+      passCon.style.display = "block";
+    }
+    if (conditions.condition3) {
+      conditionsName.special.style.color = "green";
+      nameCon.style.display = "block";
+    } else {
+      conditionsName.special.style.color = "red";
+      nameCon.style.display = "block";
+    }
+    if (conditions.condition4) {
+      conditionsPass.special.style.color = "green";
+      passCon.style.display = "block";
+    } else {
+      conditionsPass.special.style.color = "red";
+      passCon.style.display = "block";
+    }
+
+    if (
+      conditions.condition1 &&
+      conditions.condition2 &&
+      conditions.condition3 &&
+      conditions.condition4
+    ) {
+      const userInfo = {
+        userName,
+        userPass,
+      };
+      localStorage.setItem("userInfo", JSON.stringify(userInfo));
+      window.location.href = "/start-page/startpage.html";
+    }
+  } else {
+    alert("please enter password and user name");
+  }
+});
+
+
+showHidePass.addEventListener("click", () => {
+  userPassInput.type = userPassInput.type === "password" ? "text" : "password";
+  img.src = img.src.includes("view.png") ? "/assets/images/hidden.png" : "/assets/images/view.png";
 });
